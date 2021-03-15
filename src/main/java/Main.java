ed33038f242bcfaf71456a4215d9571bf2f20141
Maxim Nefedov <max-ipad@mail.ru>
2021-03-15 15:39:51 +0300

    add reverse string by deque

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,26 @@
-import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
         String input = "Maxim Nefedov";
-        char[] chars = input.toCharArray();
-        char[] resultChars = new char[chars.length];
+        System.out.println(reverseString(input));
+    }
 
-        for (int i = 0; i < resultChars.length; i++) {
-            resultChars[i] = chars[(resultChars.length - 1) - i];
+    private static String reverseString(String input) {
+        char[] charArray = input.toCharArray();
+        Deque<Character> characters = new LinkedList<>();
+        for (char c : charArray) {
+            characters.offer(c);
         }
-        System.out.println(Arrays.toString(resultChars));
+
+        char[] resultArray = new char[charArray.length];
+        for (int i = 0; i < resultArray.length; i++) {
+            if (characters.getLast() != null) {
+                resultArray[i] = characters.pollLast();
+            }
+        }
+
+        return new String(resultArray);
     }
 }
