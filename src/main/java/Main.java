bfb6a21aca58708f6a7da98e077c28714a13fa6e
Maxim Nefedov <max-ipad@mail.ru>
2021-03-15 15:22:11 +0300

    create special method for reverse process

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,16 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         String input = "Maxim Nefedov";
+        System.out.println(input);
+        System.out.println(reverseString(input));
+    }
+
+    private static String reverseString(String input) {
         char[] chars = input.toCharArray();
         char[] resultChars = new char[chars.length];
-
         for (int i = 0; i < resultChars.length; i++) {
             resultChars[i] = chars[(resultChars.length - 1) - i];
         }
-        System.out.println(Arrays.toString(resultChars));
+        return new String(resultChars);
     }
 }
